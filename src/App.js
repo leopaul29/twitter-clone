a4c5945dbca875352edf9c626158a7058d5eeb56
leo <leopaulmartin@hotmail.fr>
2020-08-18 14:49:13 +0900

    create widget component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ import Feed from "./Feed";
 import Widgets from "./Widgets";
 
 function App() {
-  return (
-    // BEM
-    <div className="app">
-      {/* sidebar */}
-      <Sidebar/>
-
-      {/* feed */}
-      <Feed/>
-
-      {/* widget */}
-      <Widgets/>
-    </div>
-  );
+    return (
+        // BEM
+        <div className="app">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+        </div>
+    );
 }
 
 export default App;
