760432a92af364fb6836273b951bd95a43e27ca9
Leo-paul <leopaulmartin@hotmail.fr>
2020-12-27 15:50:07 +0100

    add github corner link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
+import GithubCorner from "react-github-corner";
+
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
 
 function App() {
-    return (
-        // BEM
-        <div className="app">
-            <Sidebar />
-            <Feed />
-            <Widgets />
-        </div>
-    );
+  return (
+    // BEM
+    <div className="app">
+      <GithubCorner
+        href="https://github.com/leopaul29/twitter-clone"
+        bannerColor="#50b7f5"
+        octoColor="#fff"
+        size={80}
+        direction="right"
+      />
+      <Sidebar />
+      <Feed />
+      <Widgets />
+    </div>
+  );
 }
 
 export default App;
