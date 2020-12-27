18a38777278d7b7f41dc981d6637d29a187060dd
Leo-paul <leopaulmartin@hotmail.fr>
2020-12-27 17:35:50 +0100

    fix: open github in new tab + deploy

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ import Feed from "./Feed";
 import Widgets from "./Widgets";
 
 function App() {
+  const githubCornerUrl = "https://github.com/leopaul29/twitter-clone"
   return (
     // BEM
     <div className="app">
       <GithubCorner
-        href="https://github.com/leopaul29/twitter-clone"
+        href={githubCornerUrl}
         bannerColor="#50b7f5"
         octoColor="#fff"
         size={80}
         direction="right"
+        target="_blank"
+        rel="noreferrer"
       />
       <Sidebar />
       <Feed />
